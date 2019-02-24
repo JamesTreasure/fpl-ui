26daf025bbabcbb3dc5c934951849e9b43c82401
james <jandrewtreasure@gmai.com>
2019-02-24 08:35:16 +0000

    message

--- src/Rewrite.js
+++ src/Rewrite.js
@@ -10,11 +10,12 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import SearchBar from 'material-ui-search-bar'
+import Button from '@material-ui/core/Button';
 
 
 
 const PATH_BASE = 'https://fpl-spring-boot.herokuapp.com/'
-const PATH_LEAGUE = '/league/670123'
+const PATH_LEAGUE = '/league/'
 const CURRENT_GAMEWEEK_EVENT = '/event/'
 const CURRENT_GAMEWEEK = '/currentGameweek'
 
@@ -36,6 +37,7 @@ class App extends Component {
     this.loadCurrentGameweekPicksForEachLeagueMember = this.loadCurrentGameweekPicksForEachLeagueMember.bind(this);
     this.calculateCurrentGameweekScoreForEachLeagueMember = this.calculateCurrentGameweekScoreForEachLeagueMember.bind(this);
     this.state = {
+      leagueId: '', 
       currentGameweek: null,
       currentGameweekPicks: {},
       currentGameweekPoints: null,
@@ -64,6 +66,7 @@ class App extends Component {
 
   calculateCurrentGameweekScoreForEachLeagueMember(j){
     var gameweekScore = 0;
+    var liveBonusPoints = 0;
     for(var i = 0; i < 11; i++){
       var pick = this.state.league.standings.results[j].currentGameweekPicks.picks[i];
       var points = this.state.currentGameweekEvent.elements[pick.element].stats.total_points;
@@ -78,17 +81,19 @@ class App extends Component {
   loadCurrentGameweekPointsForEachUser(){
     for (var i = 0; i < this.state.league.standings.results.length; i++) {
       this.loadCurrentGameweekPicksForEachLeagueMember(this.state.league.standings.results[i].entry, i);
-      // this.calculateCurrentGameweekScoreForEachLeagueMember(this.state.league.standings.results[i].entry);
     }
   }
 
   async loadLeagueData() {
-    let leagueDataResponse = await fetch(`${PATH_BASE}${PATH_LEAGUE}`);
+    let leagueDataResponse = await fetch(`${PATH_BASE}${PATH_LEAGUE}`+ this.state.leagueId);
     let league = await leagueDataResponse.json();
     this.setState({ league });
   }
 
   calculateTableValues() {
+    if(this.state.leagueId != null && this.state.leagueId.size > 0){
+      localStorage.setItem("leagueId", this.state.leagueId);
+    }
     this.loadLeagueData()
       .then(this.loadCurrentGameweekPointsForEachUser)
   }
@@ -98,7 +103,25 @@ class App extends Component {
   }
 
   setCurrentGameweekEvent(currentGameweekEvent){
+    for(var i = 0; i < currentGameweekEvent.fixtures.length; i++){
+      // if(!currentGameweekEvent.fixtures[i].finished && currentGameweekEvent.fixtures[i].started){
+      if(currentGameweekEvent.fixtures[i].finished && currentGameweekEvent.fixtures[i].started){
+        var bonusPoints = {};
+        let homeBonusPoints = currentGameweekEvent.fixtures[i].stats[9]["bps"]["a"];
+        let awayBonusPoints = currentGameweekEvent.fixtures[i].stats[9]["bps"]["a"];
+        for (let [key, value] of Object.entries(homeBonusPoints)) {
+          bonusPoints[value["element"]] = value["value"];
+          console.log(key, value);
+        }
+        for (let [key, value] of Object.entries(awayBonusPoints)) {
+          bonusPoints[value["element"]] = value["value"];
+          console.log(key, value);
+        }
+        currentGameweekEvent.fixtures[i].stats[9]["bps"]["a"]
+      }
+    }
     this.setState({currentGameweekEvent})
+    
   }
 
   async preload(){
@@ -109,33 +132,49 @@ class App extends Component {
     let currentGameweekEventResponse = await fetch(`${PATH_BASE}${CURRENT_GAMEWEEK_EVENT}` + this.state.currentGameweek.currentGameweek);
     let currentGameweekEvent = await currentGameweekEventResponse.json();
     this.setCurrentGameweekEvent(currentGameweekEvent);
+
+    if(localStorage.leagueId != null && localStorage.leagueId.length > 0){
+      this.setState({leagueId:localStorage.leagueId})
+      this.calculateTableValues();
+    }
   }
 
   componentDidMount() {
     this.preload();
   }
 
+  randomButton(){
+    return (
+    <Button variant="contained">
+    Default
+  </Button>)
+  }
+
   searchButton() {
     return (
       <SearchBar
-        onChange={() => console.log(this.state)}
+        placeholder={this.state.leagueId}
+        value={this.state.value}
+        onChange={(query) => {this.setState({leagueId:query})}}
         onRequestSearch={() => this.calculateTableValues()}
         style={{
-          margin: '0 auto',
-          maxWidth: 800,
+          margin: '5 5 5 5'
         }}
       />
     );
   }
 
   render() {
-    if (this.state.league.standings == null) {
+    if (this.state.currentGameweek == null || this.state.currentGameweekEvent == null || this.state.league.standings == null) {
       return this.searchButton();
     }
 
     return (
       <div className="App">
-        {this.searchButton()}
+        <div class="wrapper">
+        <div id = "one">{this.searchButton()}</div>
+        <div id = "two">{this.randomButton()}</div>
+        </div>
         <Paper>
           <Table>
             <TableHead>
@@ -144,6 +183,7 @@ class App extends Component {
                 <TableCell numeric>Player Name</TableCell>
                 <TableCell numeric>Points Before Current Gameweek</TableCell>
                 <TableCell numeric>Current Gameweek Points</TableCell>
+                <TableCell numeric>Live Bonus Points</TableCell>
                 <TableCell numeric>Total Points</TableCell>
               </TableRow>
             </TableHead>
@@ -157,6 +197,7 @@ class App extends Component {
                     <TableCell numeric>{item.player_name}</TableCell>
                     <TableCell numeric>{item.total - item.event_total}</TableCell>
                     <TableCell numeric>{item.current_gameweek_points}</TableCell>
+                    <TableCell numeric>{item.live_bonus_points}</TableCell>
                     <TableCell numeric>{item.total - item.event_total + item.current_gameweek_points}</TableCell>
                   </TableRow>
                 );
